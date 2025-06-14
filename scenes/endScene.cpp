6d7121f2f2e53f60432ef20f5ece196779c59163
Joo Heejeong <joohee040704@gmail.com>
2025-06-15 03:52:03 +0900

    Create endScene
    게임 종료 화면

---
+++ scenes/endScene.cpp
@@ -0,0 +1,15 @@
+#include <openGLCD.h>
+#include <openGLCD_Buildinfo.h>
+#include <openGLCD_Config.h>
+
+#include "../textUtils.h"
+
+#include "endScene.h"
+
+void showEndScene() {
+    drawCenteredText(20, "endScene");
+}
+
+void updateEndScene() {
+    
+}
---
+++ scenes/endScene.h
@@ -0,0 +1,11 @@
+#ifndef ENDSCENE_H
+#define ENDSCENE_H
+
+#include "../sceneManager.h"
+
+extern Scene currentScene;
+
+void showEndScene();
+void updateEndScene();
+
+#endif
